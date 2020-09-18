bce7085aca8ed9fdf3e1d9995bf60d7b2b2be1b2
palinalif <kollakleina@hotmail.com>
2020-09-18 11:15:46 +0000

    Finished project, untested

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,15 +1,27 @@
 # git clone https://github.com/rognvaldur20/tile_traveller.git
 
-<<<<<<< HEAD
-
-print("Pálína var hérna")
-print("Pálína var hérna aftur")
-=======
 x = 1
 y = 1
 def possible_moves(x,y):
+
     if x ==  1 and y == 1:
-        print ("You can travel: (E)ast or (S)outh.")
+        print ("You can travel: (N)orth.")
+    if x == 1 and y == 2:
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+    if x == 1 and y == 3:
+        print("You can travel: (E)ast or (S)outh.")
+    if x == 2 and y == 3:
+        print("You can travel: (E)ast or (W)est.")
+    if x == 3 and y == 3:
+        print("You can travel: (W)est or (S)outh.")
+    if x == 3 and y == 2:
+        print("You can travel: (N)orth or (S)outh.")
+    if x == 3 and y == 1:
+        print("You can travel: (N)orth.")
+    if x == 2 and y == 2:
+        print("You can travel: (W)est or (S)outh.")
+    if x == 2 and y == 1:
+        print("You can travel: (N)orth.")
     #gera fyrir alla reitina?
 
 def east(x,y):
@@ -17,27 +29,48 @@ def east(x,y):
         print("Not a valid direction!")
     elif x == 1 and y == 1:
         print("Not a valid direction!")
-    elif x == 2 and (y == 3 or y == 1):
+    elif x == 2 and (y == 2 or y == 1): #changed the first y part to 2 instead of 3, as they can move east from 2,3 but not 2,2
         print("Not a valid direction!")
     else:
         x += 1
         return(x,y)
 
 def south(x,y):
-    pass
+    if y == 1:
+        print("Not a valid direction!")
+    if x == 2 and y == 3:
+        print("Not a valid direction!")
+    else:
+        y -= 1
+        return(x, y)
 
 def north(x,y):
-    pass
+    if y == 3:
+        print("Not a valid direction!")
+    if x == 2 and y == 2:
+        print("Not a valid direction!")
+    else:
+        y+= 1
+        return(x, y)
 
 def west(x,y):
-    pass
+    if x == 1:
+        print("Not a valid direction!")
+    if x == 3 and (y == 2 or y == 1):
+        print("Not a valid direction!")
 
 while (x != 3) and (y != 1):
     possible_moves(x,y)
-    user_input = input("where u wanna go?")
-    if user_input == "e" or user_input == "E":
-        x = east(x,y)
-        #o.s.frv
+    user_input = input("Direction: ")
+    if user_input.lower() == "e":
+        east(x,y)
+    if user_input.lower() == "w":
+        west(x,y)
+    if user_input.lower() == "n":
+        north(x,y)
+    if user_input.lower() == "s":
+        south(x,y)
+    else:
+        print("Not a valid direction!")
 
-print("Victory")
->>>>>>> 5bc9372e05c8b37be93c247e7e2cd6e046ac30f3
+print("Victory!")
